888acf3ec9d49c4498827f1e25829bf23ff89e9e
drfailer <drfailer.42@gmail.com>
2024-11-17 12:24:15 -0500

    update the symbol table for the new type system

--- src/symtable/contextmanager.cpp
+++ src/symtable/contextmanager.cpp
@@ -1,35 +1,38 @@
 #include "contextmanager.hpp"
 
 std::shared_ptr<Symtable> const ContextManager::getScope() const {
-        return currentScope;
+    return currentScope;
 }
 
 void ContextManager::enterScope() {
-        auto newScope = std::make_shared<Symtable>(currentScope);
-        currentScope->addScope(newScope);
-        currentScope = newScope;
+    auto newScope = std::make_shared<Symtable>(currentScope);
+    currentScope->addScope(newScope);
+    currentScope = newScope;
 }
 
 void ContextManager::leaveScope() { currentScope = currentScope->getFather(); }
 
-void ContextManager::newSymbol(std::string name, std::list<PrimitiveType> type,
+void ContextManager::newSymbol(std::string const &name, type_system::type type,
                                Kind kind) {
-        currentScope->add(name, type, kind);
+    currentScope->add(name, type, kind);
 }
 
-void ContextManager::newSymbol(std::string name, std::list<PrimitiveType> type,
+void ContextManager::newSymbol(std::string const &name, type_system::type type,
                                unsigned int size, Kind kind) {
-        currentScope->add(name, type, size, kind);
+    currentScope->add(name,
+                      type_system::make_type<type_system::StaticArray>(
+                          type_system::getElementType(type), size),
+                      kind);
 }
 
 /**
  * trick for functions
  */
-void ContextManager::newGlobalSymbol(std::string name, std::list<PrimitiveType> type,
+void ContextManager::newGlobalSymbol(std::string name, type_system::type type,
                                      Kind kind) {
-        globalScope->add(name, type, kind);
+    globalScope->add(name, type, kind);
 }
 
 std::optional<Symbol> ContextManager::lookup(std::string name) const {
-        return currentScope->lookup(name);
+    return currentScope->lookup(name);
 }
--- src/symtable/contextmanager.hpp
+++ src/symtable/contextmanager.hpp
@@ -1,25 +1,26 @@
 #ifndef CONTEXT_MANAGER_H
 #define CONTEXT_MANAGER_H
 #include "symtable/symtable.hpp"
-#include <list>
 
 class ContextManager {
-      public:
-        ContextManager() :
-          currentScope(std::make_shared<Symtable>()),
+  public:
+    ContextManager()
+        : currentScope(std::make_shared<Symtable>()),
           globalScope(currentScope) {}
 
-        void enterScope();
-        void leaveScope();
-        std::shared_ptr<Symtable> const getScope() const; // NOTE: may be wrong
-        void newSymbol(std::string name, std::list<PrimitiveType> type, Kind kind);
-        void newSymbol(std::string name, std::list<PrimitiveType> type, unsigned int size, Kind kind);
-        void newGlobalSymbol(std::string name, std::list<PrimitiveType> type, Kind kind);
-        std::optional<Symbol> lookup(std::string name) const;
+    void enterScope();
+    void leaveScope();
+    std::shared_ptr<Symtable> const getScope() const; // NOTE: may be wrong
+    void newSymbol(std::string const &name, type_system::type type, Kind kind);
+    void newSymbol(std::string const &name, type_system::type type,
+                   unsigned int size, Kind kind);
+    void newGlobalSymbol(std::string name, type_system::type type,
+                         Kind kind);
+    std::optional<Symbol> lookup(std::string name) const;
 
-      private:
-        std::shared_ptr<Symtable> currentScope = nullptr;
-        std::shared_ptr<Symtable> globalScope = nullptr;
+  private:
+    std::shared_ptr<Symtable> currentScope = nullptr;
+    std::shared_ptr<Symtable> globalScope = nullptr;
 };
 
 #endif
