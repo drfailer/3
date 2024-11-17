8fb3574e6a7bc871f248ff2f99d35c23f9800c1d
drfailer <drfailer.42@gmail.com>
2024-11-17 12:23:14 -0500

    new types

--- src/typesystem/types.cpp
+++ src/typesystem/types.cpp
@@ -1,8 +1,9 @@
 #include "types.hpp"
 #include <iostream>
-#include <list>
 
-std::ostream &operator<<(std::ostream &os, const PrimitiveType &type) {
+namespace type_system {
+
+std::ostream &operator<<(std::ostream &os, PrimitiveTypes type) {
     switch (type) {
     case INT:
         os << "int";
@@ -13,82 +14,106 @@ std::ostream &operator<<(std::ostream &os, const PrimitiveType &type) {
     case CHR:
         os << "chr";
         break;
-    case ARR_INT:
-        os << "int[]";
-        break;
-    case ARR_FLT:
-        os << "flt[]";
-        break;
-    case ARR_CHR:
-        os << "chr[]";
-        break;
     default:
-        os << "void";
+        os << "nil";
         break;
     }
     return os;
 }
 
-std::ostream &operator<<(std::ostream &os, const std::list<PrimitiveType> types) {
-    for (PrimitiveType type : types) {
-        os << type << " -> ";
+std::ostream &operator<<(std::ostream &os, types const &types) {
+    auto it = types.cbegin();
+
+    os << "(" << (*it++)->toString();
+    for (; it != types.cend(); it++) {
+        os << ", " << (*it)->toString();
     }
-    os << ".";
+    return os << ")";
+}
+
+std::ostream &operator<<(std::ostream &os, Function const &type) {
+    os << type.returnType << type.argumentsTypes;
     return os;
-}
-
-PrimitiveType getArrayType(PrimitiveType type) {
-    switch (type) {
-    case INT:
-        return ARR_INT;
-        break;
-    case FLT:
-        return ARR_FLT;
-        break;
-    case CHR:
-        return ARR_CHR;
-        break;
-    default:
-        return type;
-        break;
-    }
-}
-
-PrimitiveType getValueType(PrimitiveType type) {
-    switch (type) {
-    case ARR_INT:
-        return INT;
-        break;
-    case ARR_FLT:
-        return FLT;
-        break;
-    case ARR_CHR:
-        return CHR;
-        break;
-    default:
-        return type;
-        break;
-    }
-}
-
-bool isArray(PrimitiveType type) {
-    return type == ARR_INT || type == ARR_FLT || type == ARR_CHR;
-}
-
-bool isNumber(PrimitiveType type) {
-    PrimitiveType typeToCheck = getValueType(type);
-    return typeToCheck == INT || typeToCheck == FLT;
 }
 
 /**
  * @brief  Automatic convertion to flt in operators (flt op int).
  */
-PrimitiveType selectType(PrimitiveType left, PrimitiveType right) {
-    PrimitiveType type;
-    if (left == INT && right == INT) {
-        type = INT;
-    } else {
-        type = FLT;
+type selectType(type left, type right) {
+    PrimitiveTypes primitiveLeft, primitiveRight;
+    type result;
+    types arguments = {left, right};
+
+    primitiveLeft = left->getEvaluatedType();
+    primitiveRight = right->getEvaluatedType();
+    if (primitiveLeft == INT && primitiveRight == INT) {
+        return make_type<Function>(make_type<Primitive>(INT), arguments);
     }
-    return type;
+    return make_type<Function>(make_type<Primitive>(FLT), arguments);
 }
+
+bool isArray(type t) {
+    return t->kind == TypeKinds::StaticArray ||
+           t->kind == TypeKinds::DynamicArray;
+}
+
+size_t getArraySize(type const t) {
+    auto arrayType = std::static_pointer_cast<StaticArray>(t);
+
+    if (!arrayType) {
+        throw std::runtime_error("error: try to use getArraySize on a type "
+                                 "that is not a static array.");
+    }
+    return arrayType->size;
+}
+
+// todo: array might store any type in the future
+PrimitiveTypes getElementType(type const t) {
+    switch (t->kind) {
+    case TypeKinds::Primitive: {
+        auto primitive = std::static_pointer_cast<Primitive>(t);
+        return primitive->type;
+    } break;
+    case TypeKinds::StaticArray: {
+        auto staticArray = std::static_pointer_cast<StaticArray const>(t);
+        return staticArray->elementType;
+    } break;
+    case TypeKinds::DynamicArray: {
+        auto dynamicArray = std::static_pointer_cast<DynamicArray const>(t);
+        return dynamicArray->elementType;
+    } break;
+    default:
+        break;
+        ;
+    }
+    return NIL;
+}
+
+bool isArrayOfChr(type const t) { return getElementType(t) == CHR; }
+
+bool isNumber(type const t) {
+    switch (t->kind) {
+    case TypeKinds::Primitive: {
+        auto primitiveType = std::static_pointer_cast<Primitive const>(t);
+        return primitiveType->type == INT || primitiveType->type == FLT;
+    } break;
+    default:
+        break;
+        ;
+    }
+    return false;
+}
+
+type getReturnType(type const t) {
+    switch (t->kind) {
+    case TypeKinds::Function: {
+        auto functionType = std::static_pointer_cast<Function>(t);
+        return functionType->returnType;
+    } break;
+    default:
+        break;
+    }
+    return make_type<Primitive>(NIL);
+}
+
+} // end namespace type_system
--- src/typesystem/types.hpp
+++ src/typesystem/types.hpp
@@ -2,7 +2,12 @@
 #define TYPES_H
 #include <iostream>
 #include <list>
+#include <map>
+#include <memory>
+#include <sstream>
 #define MAX_LITERAL_STRING_LENGTH 1000
+
+namespace type_system {
 
 union LiteralValue {
     long long _int;
@@ -11,23 +16,161 @@ union LiteralValue {
     char _str[MAX_LITERAL_STRING_LENGTH];
 };
 
-enum PrimitiveType { NIL, INT, FLT, CHR, ARR_INT, ARR_FLT, ARR_CHR, OBJ };
-enum ArrayType { STATIC, DYNAMIC, NONE };
+enum PrimitiveTypes { NIL, INT, FLT, CHR };
+enum class TypeKinds { Primitive, StaticArray, DynamicArray, Obj, Function };
 
 struct Type {
-    PrimitiveType primitiveType = NIL; //< primitive type or composed type
-    ArrayType arrayType = NONE;        //< static, dynamic array or none
-    std::list<Type> types = {};        //< list of types for composed types
+    Type(TypeKinds kind) : kind(kind) {}
+    virtual ~Type() {}
+    virtual std::string toString() const = 0;
+    virtual PrimitiveTypes getEvaluatedType() const = 0;
+    virtual bool compare(std::shared_ptr<Type> const) const = 0;
+    TypeKinds kind;
 };
 
-PrimitiveType getArrayType(PrimitiveType type);
-PrimitiveType getValueType(PrimitiveType type);
-bool isArray(PrimitiveType type);
-bool isNumber(PrimitiveType type);
+using type = std::shared_ptr<Type>;
+using types = std::list<type>;
 
-std::ostream &operator<<(std::ostream &os, const PrimitiveType &type);
-std::ostream &operator<<(std::ostream &os, const std::list<PrimitiveType> types);
+template <typename T, typename... Args>
+std::shared_ptr<T> make_type(Args &&...args) {
+    return std::make_shared<T>(std::forward<Args>(args)...);
+}
 
-PrimitiveType selectType(PrimitiveType left, PrimitiveType right);
+std::ostream &operator<<(std::ostream &os, PrimitiveTypes type);
+std::ostream &operator<<(std::ostream &os, types const &types);
+
+struct Primitive : Type {
+    Primitive(PrimitiveTypes type = INT)
+        : Type(TypeKinds::Primitive), type(type) {}
+    PrimitiveTypes type;
+
+    std::string toString() const override {
+        std::ostringstream oss;
+        oss << type;
+        return oss.str();
+    }
+
+    PrimitiveTypes getEvaluatedType() const override { return type; }
+
+    bool compare(std::shared_ptr<Type> const other) const override {
+        if (other->kind != TypeKinds::Primitive) {
+            return false;
+        }
+        auto otherPrimitive = std::static_pointer_cast<Primitive>(other);
+        return otherPrimitive->type == type;
+    }
+};
+
+struct StaticArray : Type {
+    StaticArray(PrimitiveTypes elementType = INT, size_t size = 0)
+        : Type(TypeKinds::StaticArray), elementType(elementType), size(size) {}
+    PrimitiveTypes elementType;
+    size_t size;
+
+    std::string toString() const override {
+        std::ostringstream oss;
+        oss << elementType << "[" << size << "]";
+        return oss.str();
+    }
+
+    PrimitiveTypes getEvaluatedType() const override { return elementType; }
+
+    bool compare(std::shared_ptr<Type> const other) const override {
+        if (other->kind != TypeKinds::StaticArray) {
+            return false;
+        }
+        auto otherStaticArray = std::static_pointer_cast<StaticArray>(other);
+        return otherStaticArray->elementType == elementType &&
+               otherStaticArray->size == size;
+    }
+};
+
+struct DynamicArray : Type {
+    DynamicArray(PrimitiveTypes elementType)
+        : Type(TypeKinds::DynamicArray), elementType(elementType) {}
+    PrimitiveTypes elementType;
+
+    std::string toString() const override {
+        std::ostringstream oss;
+        oss << elementType << "[dyn]";
+        return oss.str();
+    }
+
+    PrimitiveTypes getEvaluatedType() const override { return elementType; }
+
+    bool compare(std::shared_ptr<Type> const other) const override {
+        if (other->kind != TypeKinds::DynamicArray) {
+            return false;
+        }
+        auto otherDynamicArray = std::static_pointer_cast<DynamicArray>(other);
+        return otherDynamicArray->elementType == elementType;
+    }
+};
+
+struct Obj : Type {
+    Obj(std::string const &name, std::map<std::string, type> const &types)
+        : Type(TypeKinds::Obj), name(name), types(types) {}
+    std::string name;
+    std::map<std::string, type> types = {};
+
+    std::string toString() const override { return name; }
+
+    PrimitiveTypes getEvaluatedType() const override {
+        throw std::runtime_error("todo: how to handle returned structs");
+        return NIL;
+    }
+};
+
+struct Function : Type {
+    Function(type returnType = make_type<Primitive>(NIL),
+             types const &argumentsTypes = {})
+        : Type(TypeKinds::Function), returnType(returnType),
+          argumentsTypes(argumentsTypes) {}
+    type returnType = nullptr;
+    types argumentsTypes = {};
+
+    std::string toString() const override {
+        std::ostringstream oss;
+        oss << returnType->toString() << argumentsTypes;
+        return oss.str();
+    }
+
+    PrimitiveTypes getEvaluatedType() const override {
+        return returnType->getEvaluatedType();
+    }
+
+    bool compare(std::shared_ptr<Type> const other) const override {
+        if (other->kind != TypeKinds::Function) {
+            return false;
+        }
+        auto otherFunction = std::static_pointer_cast<Function>(other);
+        if (!otherFunction->returnType->compare(returnType)) {
+            return false;
+        }
+
+        auto thisIt = argumentsTypes.cbegin();
+        auto thisEnd = argumentsTypes.cend();
+        auto otherIt = otherFunction->argumentsTypes.cbegin();
+        auto otherEnd = otherFunction->argumentsTypes.cend();
+        for (; otherIt != otherEnd; otherIt++) {
+            if (thisIt == thisEnd || !(*thisIt++)->compare(*otherIt)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
+
+type selectType(type left, type right);
+bool isArray(type t);
+bool isArrayOfChr(type const t);
+size_t getArraySize(type const t);
+PrimitiveTypes getElementType(type const t);
+PrimitiveTypes getValueType(type const t);
+bool isNumber(type const t);
+type getReturnType(type const t);
+
+} // end namespace type_system
 
 #endif
