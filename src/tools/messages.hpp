a87c84df2db143bfadaf12b096142f776b584910
drfailer <drfailer.42@gmail.com>
2025-07-26 19:06:24 -0400

    new messages

--- src/tools/messages.hpp
+++ src/tools/messages.hpp
@@ -32,13 +32,16 @@ void warning(std::string const &msg);
 #define QUOTE(val) "'" BOLD << val << NORM "'"
 
 #define UNDEFINED_SYMBOL_ERROR(loc, symbol_id)                                 \
-    ERROR(loc, "undefined symbol " << QUOTE(symbol_id) << ".")
+    ERROR(loc, "undefined symbol " << QUOTE(symbol_id) << ". " << __FILE__ << " " << __LINE__)
 #define MULTIPLE_DEFINITION_ERROR(loc, name, symbol_location)                  \
     ERROR(loc, "redifinition definition of symbol "                            \
                    << QUOTE(name) << " (previously defined here: '"            \
                    << QUOTE(symbol_location) << "').")
+// TODO: this message could be improved
 #define ARITHMETIC_OPERATOR_ERROR(loc, operator_name)                          \
     ERROR(loc, "bad usage of operator " << QUOTE(operator_name) << ".")
+#define INVALID_CALL_ERROR(loc, id)                                            \
+    ERROR(loc, "invalide call to non function object " << QUOTE(id) << ".")
 // TODO: need a function to print type list
 #define NON_MATCHING_FUNCTION_CALL_ERROR(loc, function_name, found, expected)  \
     ERROR(loc, "non matching funciton call to "                                \
@@ -58,9 +61,19 @@ void warning(std::string const &msg);
                    << "\n."                                                    \
                       "expected: "                                             \
                    << QUOTE(type::type_to_string(expected_type)))
+#define WRONG_NUMBER_OF_ARGUMENT_ERROR(loc, function_name, function_type)      \
+    ERROR(loc, "wrong number of argument for "                                 \
+                   << QUOTE(function_name) << " which is of type "             \
+                   << QUOTE(type::type_to_string(function_type)) << ".\n")
+
 #define ARGUMENT_TYPE_ERROR(loc, function_name, arg_idx, found_type,           \
                             expected_type)                                     \
-    TODO
+    ERROR(loc,                                                                 \
+          "wrong type for argument "                                           \
+              << arg_idx << " of function " << QUOTE(function_name) << ".\n"   \
+              << "found: " << QUOTE(type::type_to_string(found_type)) << ".\n" \
+              << "expected: " << QUOTE(type::type_to_string(expected_type)))
+
 #define INDEX_NON_ARRAY_TYPE_ERROR(loc, variable_name)                         \
     ERROR(loc, QUOTE(variable_name) << " is not indexable.")
 #define INVALID_INDEX_TYPE_ERROR(loc, found_type)                              \
@@ -74,5 +87,26 @@ void warning(std::string const &msg);
     WARNING(loc, "in " << QUOTE(function_id) << " implicit convertion from "   \
                        << QUOTE(type::type_to_string(found_type)) << " to "    \
                        << QUOTE(expected_type) << ".")
+
+#define INVALID_MOV_ERROR(loc, target_type)                                    \
+    ERROR(loc, "bad assignment of to expression of type "                      \
+                   << QUOTE(type::type_to_string(target_type)) << ".")
+#define BAD_ASSIGNMENT_ERROR(loc, found_type, expected_type)                   \
+    ERROR(loc, "bad assignment of of an expression of type "                   \
+                   << QUOTE(type::type_to_string(found_type))                  \
+                   << " to a variable of type "                                \
+                   << QUOTE(type::type_to_string(expected_type)) << ".")
+
+#define BAD_FOR_INDEX_VARIABLE_TYPE(loc, index_id, found_type)                 \
+    ERROR(loc, "variable " << QUOTE(index_id) << " is of type "                \
+                           << QUOTE(type::type_to_string(found_type))          \
+                           << " which cannot be used in a for loops.")
+
+#define FOR_RNG_ERROR(loc, rng, index_id, index_type, found_type)              \
+    ERROR(loc, "for index variable "                                           \
+                   << QUOTE(index_id) << " is of type "                        \
+                   << QUOTE(type::type_to_string(index_type)) << " but "       \
+                   << rng << " is of type "                                    \
+                   << QUOTE(type::type_to_string(found_type)))
 
 #endif
